b12fba21d45c0b9cb0e6bd1f44508175eb1c7a00
groakat <github@rennert.io>
2016-08-01 15:50:33 +0100

    changed quoting to make system call work in windows

--- cup_detection/io/image_sequence.py
+++ cup_detection/io/image_sequence.py
@@ -12,13 +12,13 @@ def clear_folder(folder):
 
 def video_to_image_sequence(video_file, output_folder):
     clear_folder(output_folder)
-    out = subprocess.Popen("ffmpeg -i '{}' '{}'".format(video_file,
+    out = subprocess.Popen('ffmpeg -i "{}" "{}"'.format(video_file,
                                             os.path.join(output_folder, "%06d.png")), shell=True).communicate()[0]
     print(out)
 
 
 def image_sequence_to_video(input_folder, video_file):
-    out = subprocess.Popen("ffmpeg -i '{}' -pix_fmt yuv420p '{}'".format(os.path.join(input_folder, "%06d.png"),
+    out = subprocess.Popen('ffmpeg -i "{}" -pix_fmt yuv420p "{}"'.format(os.path.join(input_folder, "%06d.png"),
                                                         video_file), shell=True).communicate()[0]
 
     print (out)
